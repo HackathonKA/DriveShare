4bbd178d7ba8fa5d2a75d7f82a5a8a20584c02ab
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2018-03-24 13:08:57 +0100

    Added basic log[in/out] templates

--- fg/urls.py
+++ fg/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('user/login', auth_views.LoginView.as_view()),
+    path('user/logout', auth_views.LogoutView.as_view(), {"template_name": "registration/logout.html"}),
 ]
