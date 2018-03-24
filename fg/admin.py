dda9e79ed9e535ac1335e54d601d4e9d10cdba76
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2018-03-24 16:55:57 +0100

    Added Database Models

--- fg/admin.py
+++ fg/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 1
+
+class CarpoolAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline]
+    fieldsets = [
+        (None, {"fields": ["name", "desc", "active"]}),
+        ("Locations", {"fields": ["loc_a", "loc_b"]})
+    ]
+
+admin.site.register(Carpool, CarpoolAdmin)
+admin.site.register(Car)
